409207563f1ba3f670d47d84fac59e612327514b
Ikary <ikary@LIONFIRE.google.com>
2024-10-29 10:47:56 +0000

    astro dev bug bounty 2

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,5 +6,8 @@ import react from '@astrojs/react';
 // https://astro.build/config
 export default defineConfig({
   integrations: [react()],
-  output: 'server', // Nécessaire pour les API endpoint
+  output: 'server',
+  redirects: {
+    '/': '/googledorks'
+  }
 });
